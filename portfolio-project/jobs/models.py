186cb06c94cc25d3607542d6f1a631a13ccfa84d
DevLuDaley <daleyluis@gmail.com>
2020-02-07 08:49:23 -0500

    wired 'intro' , 'category' , weblinks attributes with "default = ' ' " in models.py

--- portfolio-project/jobs/models.py
+++ portfolio-project/jobs/models.py
@@ -3,8 +3,11 @@ from django.db import models
 
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
+    intro = models.CharField(max_length=100, default='Enter Intro Here...')
     summary = models.CharField(max_length=200)
-   # url = models.CharField(max_length=200, blank=True)
+    category = models.CharField(
+        max_length=100, default='Enter category Here...')
+    weblink = models.CharField(max_length=200, default='Enter Url Here...')
 
     def __str__(self):
-        return self.summary
+        return self.intro
