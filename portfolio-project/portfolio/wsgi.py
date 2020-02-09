7e60416351020e0d077ec9726c1b1084635da459
DevLuDaley <daleyluis@gmail.com>
2020-02-09 05:35:21 -0500

    Update wsgi.py

--- portfolio-project/portfolio/wsgi.py
+++ portfolio-project/portfolio/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ["DJANGO_SETTINGS_MODULE"] = 'portfolio.settings'
+os.environ["DJANGO_SETTINGS_MODULE"] = 'portfolio-project.portfolio.settings'
 
 
 application = get_wsgi_application()
